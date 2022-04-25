9f38599d943a623ce91ad9e437fe0ed004a5819e
Noelherr96 <noelgame02@gmail.com>
2022-04-25 16:11:21 +0200

    CBC updated

--- INF143A/Mandatory 3/problem1.py
+++ INF143A/Mandatory 3/problem1.py
@@ -1,3 +1,4 @@
+from os import remove
 import block as blk
 
 """
@@ -8,7 +9,7 @@ TO DO:
 *) Implement OFB
 """
 
-# Making 32-bit IV-block
+# Making 32-bit IV-block that consists of alternating 1s and 0s
 iv = [1]
 for i in range(1, 32):
    if iv[i - 1] == 0:
@@ -25,60 +26,80 @@ for i in range(1, 32):
     else:
         key.append(0)
 
-#print(f"key: {key}, Len: {len(key)}")
 
 def bitfile_reader():
     text_blocks = []
-    placeholder = []
+    temp = []
+    final = []
     # Read the file and outputs in binary
     with open("gold_plaintext.in", mode='r') as file:
         while (byte := file.read()):
             text_blocks.append(byte)
     
-    # https://stackoverflow.com/questions/30712020/typeerror-encoding-or-errors-without-a-string-argument
+    
     for items in text_blocks:
         binary_converted = ' '.join(format(ord(c), 'b') for c in items)
-        placeholder = (binary_converted.replace(" ", ""))
-        
-    return placeholder
+        remove_spaces = (binary_converted.replace(" ", ""))
+        for j in remove_spaces:
+            temp.append(int(j))
 
+    # Splitting the list into 32-bit length chunks
+    chunks=[temp[i:i + 32] for i in range(0, len(temp), 32)]
+
+
+    for x, element in enumerate(chunks):
+        while len(element) < 32:
+            element.append(0)
+        final.append(element)
+    return final
 
 
 # ECB - Electronic codebook mode
 def ECB_mode(input):
-    # binary_lists = [input[x:x+32] for x in range(0, len(input), 32)]
-    ECB = []
-
-    # Adding commas
-    for i in input:
-        temp = []
-        for j in i:
-            ECB.append(int(j))
-
-    # Splitting the list into 32 length chunks
-    output=[ECB[i:i + 32] for i in range(0, len(ECB), 32)]
-    
-    
-    ECB_solved = []
-    for blocks in output:
-        while len(blocks) < 32:
-            blocks.append(0)
+    ECB_encrypted = []
+    for blocks in input:
         encrypt = blk.xor(blk.gold(key), blocks)
-        ECB_solved.append(encrypt)
-    return ECB_solved
+        ECB_encrypted.append(encrypt)
+    return ECB_encrypted
+
 
         
-print(ECB_mode(bitfile_reader()))
-
 #CBC - Cipherblock chaining mode
 """
 Process:
-1) Message 1 XOR with IV(initialisastion vector), you get Ciphertext 1
-2) Use Ciphertext 1 and XOR with Message 2, you get ciphertext 2
-3) Same procedure as step 2, but XOR ciptertext 2 with message 3
+1) Message 1 XOR with IV(initialisastion vector), encrypt the xor'd with key, you get Ciphertext 1
+2) Use Ciphertext 1 and XOR with Message 2, encrypt the xor'd with the key, you get Ciphertext 2
+3) Same procedure as step 2, but XOR Ciptertext 2 with Message 3, encrypt the xor'd with key, 
+4) repeat
 """
-def CBC_mode():
-    return True
+def CBC_mode(input):
+    temp = []
+    CBC_encrypted = []
+
+    n = 0
+
+    for i, element in enumerate(input):
+        #making ciphertext block nr 1
+        if i == 0:
+            iv_step = blk.xor(element, iv)
+            first_block = blk.xor(blk.gold(iv_step), key)
+            CBC_encrypted.append(first_block)
+        #making ciphertext block nr. 2
+        elif i == 1:
+            second_xor = blk.xor(element, first_block)
+            second_block = blk.xor(blk.gold(second_xor), key)
+            temp = element
+            CBC_encrypted.append(second_block)
+        #making ciphertext block n+1
+
+    for lists in CBC_encrypted:
+        xor_step = blk.xor(lists, temp)
+        next_block = blk.xor(blk.gold(xor_step), key)
+
+        CBC_encrypted.append(next_block)
+    return CBC_encrypted
+
+print(CBC_mode(bitfile_reader()))
 
 #OFB - Output feedback mode
 
