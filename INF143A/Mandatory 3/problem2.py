76575e870c570ce96c1ddb15808c074147463cac
Noel.Herrera <Noelherr96@hotmail.com>
2022-04-27 22:05:00 +0200

    More accurate byte reader fixed on problem1 and 2

--- INF143A/Mandatory 3/problem2.py
+++ INF143A/Mandatory 3/problem2.py
@@ -9,35 +9,6 @@ Problem 2. Matyas-Meyer-Oseas
 4) Hash the plaintext in gold plaintext.in using the resulting hash function.
 """
 
-def bitfile_reader():
-    text_blocks = []
-    temp = []
-    final = []
-    # Read the file and outputs in binary
-    with open("hash.out", mode='r') as file:
-        while (byte := file.read()):
-            text_blocks.append(byte)
-    
-    
-    for items in text_blocks:
-        binary_converted = ' '.join(format(ord(c), 'b') for c in items)
-        remove_spaces = (binary_converted.replace(" ", ""))
-        for j in remove_spaces:
-            temp.append(int(j))
-
-    # Splitting the list into 32-bit length chunks
-    chunks=[temp[i:i + 32] for i in range(0, len(temp), 32)]
-
-
-    for x, element in enumerate(chunks):
-        while len(element) < 32:
-            element.append(0)
-        final.append(element)
-    return final
-
-
-# print(bitfile_reader())
-
 def read_file(input_file):
     f = open(input_file, "rb")
     data = f.read()
@@ -45,30 +16,28 @@ def read_file(input_file):
 
     return data
 
-def bytes_to_bits(B):
+def bitfile_reader(B):
+    temp = []
     bits = []
     for i in range(len(B)):
         current_byte = B[i]
         mask = 128
         for j in range(8):
             if (current_byte >= mask):
-                bits.append(1)
+                temp.append(1)
                 current_byte -= mask
             else:
-                bits.append(0)
+                temp.append(0)
             mask = mask // 2
 
-    chunks=[bits[i:i + 32] for i in range(0, len(bits), 32)]
-
+    chunks=[temp[i:i + 32] for i in range(0, len(temp), 32)]
+    
     for x, element in enumerate(chunks):
         while len(element) < 32:
             element.append(0)
-        print(x, element)
         bits.append(element)
-
     return bits
 
-bytes_to_bits(read_file("hash.out"))
 
 def matyas_meyer_oseas(input):
 
